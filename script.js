7b3d8165b91fd911feac9a1602d6881098f8a666
Daehee Lee <daehee.lee308@gmail.com>
2025-02-11 17:55:58 +0900

    function playRound added. Minor error persists

--- script.js
+++ script.js
@@ -58,4 +58,73 @@ function getHumanChoice()
     return;
 }
 
-console.log(getHumanChoice());
+
+
+
+//playRound function.
+//input Param: humanChoice, computerChoice
+//if    person      computer
+//      rock        paper       Y
+//      rock        scissors    You Win! Rock beats Scissors.
+//      rock        rock        Tied. Try Again!
+//      paper       paper       Tied. Try Again!
+
+function playRound(humanChoice, computerChoice)
+{
+    if(humanChoice=="rock")
+    {
+        switch(computerChoice)
+        {
+            case "rock": return tied();
+            case "paper": return lose(humanChoice, computerChoice);
+            case "scissors": return win(humanChoice, computerChoice);
+        }
+    }
+    else if(humanChoice=="scissors")
+    {
+        switch(computerChoice)
+        {
+            case "scissors": return tied();
+            case "rock": return lose(humanChoice, computerChoice);
+            case "paper": return win(humanChoice, computerChoice);
+        }
+    }
+    else if (humanChoice=="paper")
+    {
+        switch(computerChoice)
+        {
+            case "paper": return tied();
+            case "scissors": return lose(humanChoice, computerChoice);
+            case "rock": return win(humanChoice, computerChoice);
+        }
+    }
+}
+
+
+function tied()
+{
+    return "Tied. Try Again!";
+}
+function win(humanChoice, computerChoice)
+{
+    return "You Win!" + " " + humanChoice + " beats " +computerChoice+".";
+    humanScore++;
+}
+function lose(humanChoice, computerChoice)
+{
+    return "You Lose!" + " " + computerChoice + " beats " +humanChoice+".";
+    computerScore++;
+}
+
+
+//MAIN CODE
+
+//declaring the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
